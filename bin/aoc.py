d1e58c295d072a671ffa5004868da1902adb9413
Tomasz Sobczak <tomeksobczak@gmail.com>
2022-12-19 22:30:26 +0000

    Day4 solutions

--- bin/aoc.py
+++ bin/aoc.py
@@ -5,6 +5,7 @@ import fire
 from day.day_1_report_repair import Day1
 from day.day_2_password_philosophy import Day2
 from day.day_3_toboggan_trajectory import Day3
+from day.day_4_passport_processing import Day4
 
 
 class AoC2022(object):
@@ -19,6 +20,10 @@ class AoC2022(object):
     @staticmethod
     def day_3_toboggan_trajectory():
         Day3().solve()
+
+    @staticmethod
+    def day_4_passport_processing():
+        Day4().solve()
 
 
 if __name__ == '__main__':
---
+++ src/day/day_4_passport_processing.py
@@ -0,0 +1,69 @@
+import re
+from typing import Optional
+
+from common.aoc import Task
+
+
+class Day4(Task):
+    def compute_part_one(self, version: str, return_passports: bool = False) -> str:
+        def validate_passport(d: dict) -> int:
+            if 8 == len(d) or (7 == len(d) and 'cid' not in d):
+                return 1
+            return 0
+
+        passports = self.load_passports_data(version)
+        validated = [validate_passport(p) for p in passports]
+
+        return passports if return_passports else str(sum(validated))
+
+    def compute_part_two(self, version: str) -> str:
+        def validate_passport(d: dict) -> int:
+            valid = 8 == len(d) or (7 == len(d) and 'cid' not in d)
+            valid = valid and bool(re.match(r"^\d{4}$", d['byr'])) and (1920 <= int(d['byr']) <= 2002)
+            valid = valid and bool(re.match(r"^\d{4}$", d['iyr'])) and (2010 <= int(d['iyr']) <= 2020)
+            valid = valid and bool(re.match(r"^\d{4}$", d['eyr'])) and (2020 <= int(d['eyr']) <= 2030)
+
+            hgt = re.match(r"^(\d+)(cm|in)$", d['hgt']) if valid else False
+
+            if valid and bool(hgt):
+                hgt_min = 150 if 'cm' == hgt.group(2) else 59
+                hgt_max = 193 if 'cm' == hgt.group(2) else 76
+                valid = valid and bool(hgt) and (hgt_min <= int(hgt.group(1)) <= hgt_max)
+
+            valid = valid and bool(hgt)
+            valid = valid and bool(re.match(r"^#[\da-f]{6}$", d['hcl']))
+            valid = valid and bool(re.match(r"^amb|blu|brn|gry|grn|hzl|oth$", d['ecl']))
+            valid = valid and bool(re.match(r"^\d{9}$", d['pid']))
+
+            return 1 if valid else 0
+
+        validated = [validate_passport(p) for p in self.compute_part_one(version, True)]
+
+        return str(sum(validated))
+
+    @property
+    def part_one_expected_test_value(self) -> Optional[str]:
+        return '2'
+
+    @property
+    def part_two_expected_test_value(self) -> Optional[str]:
+        return '2'
+
+    def load_passports_data(self, version) -> list:
+        current = []
+        rawdata = []
+        outdata = []
+
+        for line in self.load(version):
+            if 0 == len(line):
+                rawdata.append(re.split(r"\s+", ' '.join(current)))
+                current = []
+                continue
+
+            current.append(line)
+        rawdata.append(re.split(r"\s+", ' '.join(current)))
+
+        for d in rawdata:
+            outdata.append({s.split(':')[0]: s.split(':')[1] for s in d})
+
+        return outdata
