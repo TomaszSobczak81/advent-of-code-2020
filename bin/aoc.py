20e0cc09c7176b6f3695b86afd5ff678186ed2b9
Tomasz Sobczak <tomeksobczak@gmail.com>
2022-12-19 20:49:59 +0000

    Day3 solution

--- bin/aoc.py
+++ bin/aoc.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
-import os
-import sys
 
 import fire
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'src')))
-
-
 from day.day_1_report_repair import Day1
 from day.day_2_password_philosophy import Day2
+from day.day_3_toboggan_trajectory import Day3
 
 
 class AoC2022(object):
@@ -19,6 +15,10 @@ class AoC2022(object):
     @staticmethod
     def day_2_password_philosophy():
         Day2().solve()
+
+    @staticmethod
+    def day_3_toboggan_trajectory():
+        Day3().solve()
 
 
 if __name__ == '__main__':
